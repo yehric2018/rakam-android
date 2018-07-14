2f1ce0669a3c43a50d3feedb2f403ccfcc9a7dd6
Burak Emre Kabakcı <emrekabakci@gmail.com>
2018-07-14 05:26:03 +0300

    Update version

--- src/main/java/io/rakam/api/Constants.java
+++ src/main/java/io/rakam/api/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final String LIBRARY = "rakam-android";
     public static final String PLATFORM = "Android";
-    public static final String VERSION = "2.7.12";
+    public static final String VERSION = "2.7.13";
 
     public static final String EVENT_BATCH_ENDPOINT = "/event/batch";
     public static final String USER_SET_PROPERTIES_ENDPOINT = "/user/batch";
